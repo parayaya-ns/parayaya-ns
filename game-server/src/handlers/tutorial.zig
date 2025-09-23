bbd21b195c67ce3c017b9420f9868a946677239a
xeon <xeon@xeondev.com>
2025-09-23 21:33:35 +0300

    feat: skip hand-holding tutorials

--- game-server/src/handlers/tutorial.zig
+++ game-server/src/handlers/tutorial.zig
@@ -5,8 +5,14 @@ const AppInterface = @import("../AppInterface.zig");
 const Allocator = std.mem.Allocator;
 const log = std.log.scoped(.tutorial);
 
-pub fn onGetTutorialReq(_: Allocator, _: *AppInterface, _: pb.GetTutorialReq) !pb.GetTutorialRsp {
-    return .{ .retcode = .RetSucc };
+pub fn onGetTutorialReq(gpa: Allocator, _: *AppInterface, _: pb.GetTutorialReq) !pb.GetTutorialRsp {
+    var rsp: pb.GetTutorialRsp = .{ .retcode = .RetSucc };
+
+    try rsp.tutorial_id_list.ensureTotalCapacity(gpa, 200);
+    for (0..100) |id| rsp.tutorial_id_list.appendAssumeCapacity(@intCast(id));
+    for (1000..1100) |id| rsp.tutorial_id_list.appendAssumeCapacity(@intCast(id));
+
+    return rsp;
 }
 
 pub fn onGetGraphicsTutorialReq(_: Allocator, _: *AppInterface, _: pb.GetGraphicsTutorialReq) !pb.GetGraphicsTutorialRsp {
